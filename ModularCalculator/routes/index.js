5c6b3c65ca4ca5ab2c094dfbee2991b9977cd721
Russell Slighton <russellscode@gmail.com>
2016-08-07 06:49:03 +0800

    Backend stuff

--- ModularCalculator/routes/index.js
+++ ModularCalculator/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Base = require('base');
+var Aerospace = require('aerospace');
 var Parse = require('parser');
 
 module.exports.Parser;
@@ -16,7 +17,27 @@ router.get('/services/calculate/', function(req, res, next) {
   var par = new Parse([Base],func);
   var resu = par.run();
   console.log(resu);
-  res.send(func);
+  res.format({
+    'text/plain': function(){
+      res.send(resu.toString());
+    }
+  })
+});
+
+router.get('/services/funcList/', function(req, res, next) {
+  var url = req.url;
+  var moduleList= url.split("?")[1];
+  var modules = moduleList.split(',');
+  var functions = []
+  for(var i = 0; i<modules.length; i++){
+    functions.push(require(modules[i]).exposedFunctions);
+  }
+  console.log(functions);
+  res.format({
+    'text/plain': function(){
+      res.send([].concat.apply([], functions));
+    }
+  })
 });
 
 module.exports = router;
