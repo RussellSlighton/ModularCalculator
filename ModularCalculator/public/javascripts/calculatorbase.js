85a2beecc529a1a84d0a8e1576e6c7fdc2e22195
sethf99 <seth.f99@gmail.com>
2016-08-06 21:19:08 +0800

    Module Workshop Front End
    Introduces the Module Workshop Front End, hardcoded in a few examples.
    ToDo: Add auto-generation of cards for more modules, storage on the
    server (backend). Add webpage for each module.

--- ModularCalculator/public/javascripts/calculatorbase.js
+++ ModularCalculator/public/javascripts/calculatorbase.js
@@ -1,11 +1,13 @@
 
 
 angular.module("BlankApp", ['ngMaterial']).controller("CalculatorControllerBase",
-    function CalculatorControllerBase($scope) {
+    function CalculatorControllerBase($scope, $mdDialog, $mdMedia) {
     	$scope.stringEquation = "0";
     	this.listOfFunctions = ["+","-","/","*","C","1","2","3"];
     	$scope.temp = "Hello";
     	$scope.started = false;
+    	$scope.status;
+    	$scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
     	$scope.onClick = function(symbol){
     		if(symbol === "C"){
@@ -22,6 +24,42 @@ angular.module("BlankApp", ['ngMaterial']).controller("CalculatorControllerBase"
     		}
     		
     	}
-    }
+
+	    $scope.showAdvanced = function(ev) {
+		    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+		    $mdDialog.show({
+		      controller: DialogController,
+		      templateUrl: 'panel.tmpl.html',
+		      parent: angular.element(document.body),
+		      targetEvent: ev,
+		      clickOutsideToClose:true,
+		      fullscreen: useFullScreen
+		    })
+		    .then(function(answer) {
+		      $scope.status = 'You said the information was "' + answer + '".';
+		    }, function() {
+		      $scope.status = 'You cancelled the dialog.';
+		    });
+		    $scope.$watch(function() {
+		      return $mdMedia('xs') || $mdMedia('sm');
+		    }, function(wantsFullScreen) {
+		      $scope.customFullscreen = (wantsFullScreen === true);
+		    });
+	  	};
+	}
+
 );
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
+
+
 
