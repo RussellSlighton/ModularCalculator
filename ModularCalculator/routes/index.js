5603ecd7b4cd5d0b42163b0af1d49fbe1e5fc5e5
Russell Slighton <russellscode@gmail.com>
2016-08-07 06:10:36 +0800

    stuff2

--- ModularCalculator/routes/index.js
+++ ModularCalculator/routes/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Base = require('base');
+var Parse = require('parser');
+
+module.exports.Parser;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Modular Calculator' });
 });
+
 router.get('/services/calculate/', function(req, res, next) {
-  res.render('index')
-  next();
-});
-router.get('/services/logic/module/', function(req, res, next) {
-  res.send()
+  var url = req.url;
+  var func= url.split("?")[1];
+  var par = new Parse([Base],func);
+  var resu = par.run();
+  console.log(resu);
+  res.send(func);
 });
 
 module.exports = router;
